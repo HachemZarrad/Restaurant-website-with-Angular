3c9b319f14fd708b0447943e2cea11165110959a
Hachem Zarrad <hachemzarrad@gmail.com>
2020-04-25 21:09:23 +0100

    Assignment 2

---
+++ src/app/shared/leader.ts
@@ -0,0 +1,9 @@
+     export class Leader {
+        id: string;
+        name: string;
+        image: string;
+        designation: string;
+        abbr: String;
+        featured: boolean;
+        description: string
+      }
